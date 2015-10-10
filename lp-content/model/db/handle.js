b8d2ddfa759af1164da4a8f267837b26be20a07c
youngerheart <435495400@qq.com>
2015-10-10 19:02:58 +0800

    增加orm引用

---
+++ lp-content/model/db/handle.js
@@ -0,0 +1,12 @@
+var DB = require('./init');
+
+module.exports = {
+  select() {},
+  selectArticle() {},
+  insert() {},
+  insertArticle() {},
+  update() {},
+  updateArticle() {},
+  remove() {},
+  removeArticle() {}
+};
