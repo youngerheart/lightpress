e4ebd031ee4cf5f19be104a81613b508644cb70d
youngerheart <435495400@qq.com>
2016-03-17 14:40:57 +0800

    增加主题模块

---
+++ server/controller/theme.js
@@ -0,0 +1,11 @@
+module.exports = {
+  add(req, res) {
+    res.status(200).send();
+  },
+  del(req, res) {
+    res.status(200).send();
+  },
+  get(req, res) {
+    res.status(200).send();
+  }
+};
